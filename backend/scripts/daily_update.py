115b74d3e815d400ea7d176dbc70fd481446aec2
Gaio Augusto Moraes dos Santos <gaioaugusto16@gmail.com>
2024-11-24 23:00:52 -0500

    added configurations for railway

--- backend/scripts/daily_update.py
+++ backend/scripts/daily_update.py
@@ -1,8 +1,21 @@
-from automateDownload import download_and_extract_csv
-from parseData import parse_data
+# from automateDownload import download_and_extract_csv
+# from parseData import parse_data
 
-# Step 1: Download the latest CSV
-download_and_extract_csv()
+# # Step 1: Download the latest CSV
+# download_and_extract_csv()
 
-# Step 2: Parse the downloaded CSV and update the database
-parse_data()
+# # Step 2: Parse the downloaded CSV and update the database
+# parse_data()
+
+from scripts.automateDownload import download_and_extract_csv
+from scripts.parseData import parse_data
+
+def run_daily_update():
+    # Step 1: Download the latest CSV
+    download_and_extract_csv()
+
+    # Step 2: Parse the downloaded CSV and update the database
+    parse_data()
+
+if __name__ == "__main__":
+    run_daily_update()
